83c2411ca75f80f5cc69a874d5cb0198f1f91aeb
Michael Friendly <friendly@yorku.ca>
2019-08-26 11:35:52 -0400

    correct an error in commit()

--- R/connect.R
+++ R/connect.R
@@ -1,6 +1,9 @@
 #' Open a MySQL connection to the milestones database
 #'
-#' This version requires that you setup a keyring password for the source `milestones`
+#' This version requires that you setup a keyring password for the service `milestones` with
+#' username `milestones`. Due to York IT security, this only works with a Yorku IP address
+#' or via VPN.
+#'
 #'
 #' @return The string representing the database connection. For convenience, this is also
 #'         stored in an environment variable, `.mstone.env$connnection`.
@@ -19,8 +22,8 @@ connect <- function() {
                           host = "setebos.ccs.yorku.ca",
                           dbname = 'milestones',
                           user = 'milestones',
-                          password = key_get('milestones',
-                                             keyring = 'milestones'))
+                          password = key_get(service = 'milestones',
+                                             username = 'milestones'))
   .mstone.env <- new.env()
   .mstone.env$connnection <- mstones_con
   mstones_con
